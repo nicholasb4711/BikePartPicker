e26628d7ff57714f2ab7817a0d2406eba41f9015
Nicholas Byrne <nicholas.byrne@whoop.com>
2025-10-02 22:05:51 -0400

    Update BuildSummary.tsx

--- apps/web/src/components/Builder/BuildSummary/BuildSummary.tsx
+++ apps/web/src/components/Builder/BuildSummary/BuildSummary.tsx
@@ -38,6 +38,7 @@ export default function BuildSummary({
                       <p className="font-semibold text-blue-600">
                         ${component.price.toLocaleString()}
                       </p>
+                      <p>Test</p>
                     </div>
                   )
                 )
